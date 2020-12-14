6ea56e4a8219fe4329bee017d9340588db9d2ab5
Mia1251 <svanteomax@gmail.com>
2020-12-14 20:17:51 +0100

    Exercise3_first week

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,37 @@
+package se.lexicon;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Calculations and result:");
+
+        System.out.println("Enter your first number:");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter your second number:");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Now choose an operation type: +. -, * or /");
+        String operationTypE= scanner.next();
+        switch (operationTypE) {
+
+            case "+":
+                int resultAdd = (number1 + number2);
+                System.out.println("The sum is " + resultAdd);
+                break;
+            case "-":
+                int resultSub = (number1 - number2);
+                System.out.println("The difference is " + resultSub);
+                break;
+            case "*":
+                int resultMul = (number1 * number2);
+                System.out.println("The end result becomes " + resultMul);
+                break;
+            case "/":
+                int resultDiv = (number1 / number2);
+                System.out.println("The end result becomes " + resultDiv);
+                break; }
+    }
+}
